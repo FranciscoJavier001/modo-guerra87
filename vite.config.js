71b061f2121ee3913d91af04f261c80be6a7f895
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2025-05-31 21:19:38 -0600

    Color Updated

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     start_url: '.',
     display: 'standalone',
     background_color: '#ffffff',
-    theme_color: '#3b82f6',
+    theme_color: '#000000',
     icons: [
       {
         src: '/icon-192.png',
